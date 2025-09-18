6ed915f581210c5ce5acf04424981e036f46a151
YassinElBouzrouti <yassinelbouzrouti.004@gmail.com>
2025-09-18 20:20:05 +0200

    menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,31 @@ const Header = () => `
  <header>
   <img src="/rafagym_logo.svg" alt="logo del gimnasio">
   <nav>
-   <ul>
+   <ul id="sidebar">
+      <li onclick=hideSidebar()><a href="#"><img src="/close.svg"</a></li>
       <li><a href="#hero">CLUB</a></li>
       <li><a href="#instalaciones">INSTALACIONES</a></li>
       <li><a href="#horarios">HORARIOS</a></li>  
       <li><a href="#contacto">CONTACTO</a></li>      
    </ul>
+   <ul>
+      <li class="hideOnMobile"><a href="#hero">CLUB</a></li>
+      <li class="hideOnMobile"><a href="#instalaciones">INSTALACIONES</a></li>
+      <li class="hideOnMobile"><a href="#horarios">HORARIOS</a></li>  
+      <li class="hideOnMobile"><a href="#contacto">CONTACTO</a></li>  
+      <li class="menu_button" onclick=showSidebar()><a href="#"><img src="/menu.svg" alt="menu"</a></li>    
+   </ul>
   </nav>
  </header>
 `
+function showSidebar() {
+   const sidebar = document.querySelector("#sidebar")
+   sidebar.style.display = "flex"
+}
+function hideSidebar() {
+   const sidebar = document.querySelector("#sidebar")
+   sidebar.style.display = "none"
+}
+
 
 export default Header;
