0d1343bd665643931ab5283647cc8c7120522aac
YassinElBouzrouti <yassinelbouzrouti.004@gmail.com>
2025-09-18 20:22:58 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => `
   <img src="/rafagym_logo.svg" alt="logo del gimnasio">
   <nav>
    <ul id="sidebar">
-      <li onclick=hideSidebar()><a href="#"><img src="/close.svg"</a></li>
+      <li onclick=hideSidebar()><a href="#"><img src="/close.svg"></a></li>
       <li><a href="#hero">CLUB</a></li>
       <li><a href="#instalaciones">INSTALACIONES</a></li>
       <li><a href="#horarios">HORARIOS</a></li>  
@@ -15,7 +15,7 @@ const Header = () => `
       <li class="hideOnMobile"><a href="#instalaciones">INSTALACIONES</a></li>
       <li class="hideOnMobile"><a href="#horarios">HORARIOS</a></li>  
       <li class="hideOnMobile"><a href="#contacto">CONTACTO</a></li>  
-      <li class="menu_button" onclick=showSidebar()><a href="#"><img src="/menu.svg" alt="menu"</a></li>    
+      <li class="menu_button" onclick=showSidebar()><a href="#"><img src="/menu.svg" alt="menu"></a></li>    
    </ul>
   </nav>
  </header>
